ad0206ee16aefb66ea08b0cfae8c625f85124e51
Kavish Arora <kavish.arora@datatusk.com>
2024-06-11 12:48:37 -0400

    update to ui

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
-console.log('This is a popup!');
+function getUrl() {
+    return window.location.href;
+}
+
+document.getElementById("checkButton").addEventListener("click", function(){
+    // RUN SCRIPT
+    var button = document.getElementById("checkButton");
+    button.parentNode.removeChild(button);
+
+    var para = document.createElement("p");
+    para.textContent = 'Checking for Malice...';
+    document.body.appendChild(para);
+});
